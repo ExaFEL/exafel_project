19ccb85ebe170616a46b5203846560f2216eea1d
vganapati <vganapa1@swarthmore.edu>
2023-03-19 23:16:46 -0700

    Update diffBragg_stage2_benchmark.sh

--- kpp-sim/diffBragg_stage2_benchmark.sh
+++ kpp-sim/diffBragg_stage2_benchmark.sh
@@ -7,7 +7,7 @@
 #SBATCH -q regular
 #SBATCH -t 00:36:00
 #SBATCH --gpus-per-node=4
-#SBATCH --ntasks-per-gpu=2
+#SBATCH --ntasks-per-gpu=8
 #SBATCH -o %j.out
 #SBATCH -e %j.err
 
@@ -18,9 +18,29 @@ export IBV_FORK_SAFE=1
 export RDMAV_HUGEPAGES_SAFE=1
 export DIFFBRAGG_USE_CUDA=1
 
+export CCTBX_DEVICE_PER_NODE=1
+export N_START=0
+export LOG_BY_RANK=1 # Use Aaron's rank logger
+export RANK_PROFILE=0 # 0 or 1 Use cProfiler, default 1
+export ADD_BACKGROUND_ALGORITHM=cuda
+export DEVICES_PER_NODE=1
+export MOS_DOM=25
+
+export CCTBX_NO_UUID=1
+export DIFFBRAGG_USE_CUDA=1
+export CUDA_LAUNCH_BLOCKING=1
+export NUMEXPR_MAX_THREADS=128
+export SLURM_CPU_BIND=cores # critical to force ranks onto different cores. verify with ps -o psr <pid>
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+export SIT_PSDM_DATA=/global/cfs/cdirs/lcls/psdm-sauter
+export CCTBX_GPUS_PER_NODE=1
+export XFEL_CUSTOM_WORKER_PATH=$MODULES/psii_spread/merging/application # User must export $MODULES path
+
+
 echo "jobstart $(date)";pwd
 
-srun -n 256 -G 128 -c 2 \
+srun -n 1024 -G 32 -c 2 \
 simtbx.diffBragg.stage_two \
 $MODULES/diffbragg_benchmarks/AD_SE_13_222/data_222.phil \
 io.output_dir=$SLURM_JOB_ID \
