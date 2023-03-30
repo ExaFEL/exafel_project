bf61d0530e83bde4f3de749887ad70c2a3ba17c0
vganapati <vganapa1@swarthmore.edu>
2023-03-30 11:05:41 -0700

    Create slurm_hopper_stage1_v3.sh

---
+++ kpp-sim/slurm_hopper_stage1_v3.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#SBATCH -N 32            # Number of nodes
+#SBATCH -J stage_1_32       # job name
+#SBATCH -L SCRATCH       # job requires SCRATCH files
+#SBATCH -A lcls_g       # allocation
+#SBATCH -C gpu
+#SBATCH -q regular
+#SBATCH -t 00:36:00
+#SBATCH --gpus-per-node=1
+#SBATCH --ntasks-per-gpu=1
+#SBATCH -o %j.out
+#SBATCH -e %j.err
+
+export SCRATCH_FOLDER=$SCRATCH/ferredoxin_sim/$SLURM_JOB_ID
+mkdir -p $SCRATCH_FOLDER; cd $SCRATCH_FOLDER
+
+export CCTBX_DEVICE_PER_NODE=1
+export N_START=0
+export LOG_BY_RANK=1 # Use Aaron's rank logger
+export RANK_PROFILE=0 # 0 or 1 Use cProfiler, default 1
+export N_SIM=500000 # total number of images to simulate
+export ADD_BACKGROUND_ALGORITHM=cuda
+export DEVICES_PER_NODE=1
+export MOS_DOM=25
+
+export CCTBX_NO_UUID=1
+export DIFFBRAGG_USE_CUDA=1
+export CUDA_LAUNCH_BLOCKING=1
+export NUMEXPR_MAX_THREADS=128
+export SLURM_CPU_BIND=cores # critical to force ranks onto different cores. verify with ps -o psr <pid>
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+export SIT_PSDM_DATA=/global/cfs/cdirs/lcls/psdm-sauter
+export CCTBX_GPUS_PER_NODE=1
+export XFEL_CUSTOM_WORKER_PATH=$MODULES/psii_spread/merging/application # User must export $MODULES path
+
+
+echo "jobstart $(date)";pwd
+
+srun -n 32 -G 32 -c 2 hopper $MODULES/exafel_project/kpp-sim/hopper_stage1.phil
+
+echo "jobend $(date)";pwd
