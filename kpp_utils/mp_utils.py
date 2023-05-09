23be98b11aefa1f5ca8071d68ff04b63eda5e3c8
Daniel Tchon <dantch@onet.eu>
2023-05-09 12:25:52 -0700

    Sendrecv in gather_dict_1by1 has some issues, try alt. impl.

--- kpp_utils/mp_utils.py
+++ kpp_utils/mp_utils.py
@@ -28,3 +28,18 @@ def gather_dict_1by1(comm, data, root=0):
     received[key] = comm.sendrecv(data[key], dest=root, source=source)
   print(f'gather3: {rank=}, {datetime.now()=}')
   return received
+
+
+def gather_dict_1by1_alt(comm, data, root=0):
+  """Gather dictionary elements by sending them one-by-one to avoid overflow"""
+  rank = comm.rank
+  print(f'gather1: {rank=}, {len(data)=}, {datetime.now()=}')
+  received = []
+  max_data_lengths = max(comm.all2all(len(data), root=root))
+  data_list = list(data.items()) + [None] * max_data_lengths
+  for index in range(max_data_lengths):
+    print(f'gather2: {index=}, {datetime.now()=}')
+    gathered = comm.gather(data_list[index], root=root)
+    received.extend(g for g in gathered if g is not None)
+  print(f'gather3: {rank=}, {len(received)=}, {datetime.now()=}')
+  return received
