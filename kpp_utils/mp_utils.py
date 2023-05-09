9d266e076b8ff038c5b4e875168c26009eca0cc6
Daniel Tchon <dantch@onet.eu>
2023-05-09 12:04:13 -0700

    Log datetime to investigate why mp helpers are so slow

--- kpp_utils/mp_utils.py
+++ kpp_utils/mp_utils.py
@@ -1,17 +1,21 @@
+from datetime import datetime
+
+
 def bcast_dict_1by1(comm, data, root=0):
   """Broadcast dictionary elements one-by-one to avoid MPI overflow issues"""
-  print(f'bcast1: {comm.rank=}, {data.items()}')
+  print(f'bcast1: {comm.rank=}, {datetime.now()=}')
   received = {}
   for key, value in data.items():
+    print(f'bcast2: {key=}, {datetime.now()=}')
     received[key] = comm.bcast([value], root=root)[0]
-  print(f'bcast2: {comm.rank=}, {received.items()}')
+  print(f'bcast3: {comm.rank=}, {datetime.now()=}')
   return received
 
 
 def gather_dict_1by1(comm, data, root=0):
   """Gather dictionary elements by sending them one-by-one to avoid overflow"""
   rank = comm.rank
-  print(f'gather1: {rank=}, {data.items()}')
+  print(f'gather1: {rank=}, {datetime.now()=}')
   received = {}
   key_rank_map = {key: rank for key in data.keys()}
   if rank == 0:
@@ -20,6 +24,7 @@ def gather_dict_1by1(comm, data, root=0):
       key_rank_map.update(key_rank_from_single_rank)
   key_rank_map = comm.bcast(key_rank_map, root=root)
   for key, source in key_rank_map.items():
+    print(f'gather2: {key=}, {datetime.now()=}')
     received[key] = comm.sendrecv(data[key], dest=root, source=source)
-  print(f'gather2: {rank=}, {received.items()}')
+  print(f'gather3: {rank=}, {datetime.now()=}')
   return received
