90d8210d3d39ed6fdb3e1f7eba73f9e2b46e7cf3
Daniel Tchon <dantch@onet.eu>
2023-05-08 19:20:15 -0700

    Correctly gather key-rank pairs in `gather_dict_1by1`

--- kpp_utils/mp_utils.py
+++ kpp_utils/mp_utils.py
@@ -1,21 +1,25 @@
 def bcast_dict_1by1(comm, data, root=0):
   """Broadcast dictionary elements one-by-one to avoid MPI overflow issues"""
-  print(f'bcast: {comm.rank=}, {data.items()}')
+  print(f'bcast1: {comm.rank=}, {data.items()}')
   received = {}
   for key, value in data.items():
     received[key] = comm.bcast([value], root=root)[0]
-  print(f'bcast: {comm.rank=}, {received.items()}')
+  print(f'bcast2: {comm.rank=}, {received.items()}')
   return received
 
 
 def gather_dict_1by1(comm, data, root=0):
   """Gather dictionary elements by sending them one-by-one to avoid overflow"""
   rank = comm.rank
-  print(f'bcast: {rank=}, {data.items()}')
+  print(f'gather1: {rank=}, {data.items()}')
   received = {}
   key_rank_map = {key: rank for key in data.keys()}
+  if rank == 0:
+    key_rank_list = comm.gather(key_rank_map, root=root)
+    for key_rank_from_single_rank in key_rank_list:
+      key_rank_map.update(key_rank_from_single_rank)
   key_rank_map = comm.bcast(key_rank_map, root=root)
   for key, source in key_rank_map.items():
     received[key] = comm.sendrecv(data[key], dest=root, source=source)
-  print(f'gather: {rank=}, {received.items()}')
+  print(f'gather2: {rank=}, {received.items()}')
   return received
