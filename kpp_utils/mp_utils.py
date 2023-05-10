d9be25715f269078396f913962b4b13ec14ed92f
Daniel Tchon <dantch@onet.eu>
2023-05-09 17:04:39 -0700

    Rename functions to accomodate both `_1by1` and `_stripes`

--- kpp_utils/mp_utils.py
+++ kpp_utils/mp_utils.py
@@ -13,7 +13,7 @@ def bcast_dict_1by1(comm, data, root=0):
   return received
 
 
-def collect_dict_1by1_alt(comm, data, root=0):
+def collect_dict_1by1(comm, data, root=0):
   """Gather dictionary elements by sending them one-by-one to avoid overflow"""
   on_root = root == comm.rank
   received = []
@@ -30,7 +30,7 @@ def collect_dict_1by1_alt(comm, data, root=0):
   return {r[0]: r[1] for r in received if r is not None} if on_root else None
 
 
-def collect_dict_1by1(comm, data, root=0):
+def collect_dict_stripes(comm, data, root=0):
   """Gather dictionary elements by sending them one-by-one to avoid overflow"""
   on_root = root == comm.rank
   received = []
