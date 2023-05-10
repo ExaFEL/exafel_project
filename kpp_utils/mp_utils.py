00cc74ada00f483e937ef9a971d06d96bec0408d
Daniel Tchon <dantch@onet.eu>
2023-05-09 17:12:53 -0700

    Assign to list instead of appending to avoid memory issues

--- kpp_utils/mp_utils.py
+++ kpp_utils/mp_utils.py
@@ -16,18 +16,20 @@ def bcast_dict_1by1(comm, data, root=0):
 def collect_dict_1by1(comm, data, root=0):
   """Gather dictionary elements by sending them one-by-one to avoid overflow"""
   on_root = root == comm.rank
-  received = []
   max_data_length = comm.reduce(len(data), op=MPI.MAX, root=root)
   max_data_length = comm.bcast(max_data_length, root=root)
   data_list = list(data.items()) + [None] * max_data_length
-  for data_row in range(max_data_length):
-    for source in range(comm.size):
+  received = [None] * max_data_length * (comm.size - 1)
+  for i, row in enumerate(range(max_data_length)):
+    for j, source in enumerate(range(1, comm.size)):
+      ij = i * (comm.size - 1) + j
       comm.barrier()
       print(f'sending from {source=} to {root=}')
-      recv = comm.sendrecv(data_list[data_row], dest=root, source=source)
-      if on_root and recv is not None:
-        received.append(recv)
-  return {r[0]: r[1] for r in received if r is not None} if on_root else None
+      received[ij] = comm.sendrecv(data_list[row], dest=root, source=source)
+      print(f'received from {source=} to {root=}')
+  if on_root:
+    data.update({r[0]: r[1] for r in received if r is not None})
+  return data if on_root else None
 
 
 def collect_dict_stripes(comm, data, root=0):
