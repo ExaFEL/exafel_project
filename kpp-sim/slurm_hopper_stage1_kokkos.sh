1e4778802b10ee4ed9061c273a58a14051ab557b
vganapati <vganapa1@swarthmore.edu>
2023-05-29 23:36:14 -0700

    Update slurm_hopper_stage1_kokkos.sh

--- kpp-sim/slurm_hopper_stage1_kokkos.sh
+++ kpp-sim/slurm_hopper_stage1_kokkos.sh
@@ -6,7 +6,7 @@
 #SBATCH -A m2859_g       # allocation
 #SBATCH -C gpu
 #SBATCH -q regular
-#SBATCH -t 00:36:00
+#SBATCH -t 03:36:00
 #SBATCH --gpus-per-node=4
 #SBATCH --ntasks-per-gpu=2
 #SBATCH -o %j.out
