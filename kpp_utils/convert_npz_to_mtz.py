25f82619d106cccccb9dd2566417e6145ebeebae
Vidya Ganapati <vganapati@gmail.com>
2023-06-22 14:27:21 -0700

    optimizing structure factors

--- kpp_utils/convert_npz_to_mtz.py
+++ kpp_utils/convert_npz_to_mtz.py
@@ -31,21 +31,25 @@ def npz_to_mtz(npz_path,
     mset = miller.set(cs, miller_idx, True)
     mdat = flex.double(f)
     ma = miller.array(mset,mdat)
-    ma.is_xray_amplitude_array()
+    # ma.is_xray_amplitude_array()
     ma = ma.set_observation_type_xray_amplitude()
+    # print(npz_path, ma.size(), np.max(ma.data()), np.mean(ma.data()), np.std(ma.data()))
     if save_mtz:
         ma.as_mtz_dataset(column_root_label='F').mtz_object().write(npz_path + '.mtz')
     return ma
 
 
 def evaluate_iter(ma,
+                  ma_proc, # output from conventional merging
                   ma_calc, # ground truth structure factors
                   show_fig=False,
                  ):
     ma_map={h:v for h,v in zip(ma.indices(),ma.data())}
     ma_calc_map={h:v for h,v in zip(ma_calc.indices(),ma_calc.data())}
+    ma_proc_map={h:v for h,v in zip(ma_proc.indices(),ma_proc.data())}
 
     hm_comm = set(ma_calc_map).intersection(set(ma_map))
+    hm_comm = hm_comm.intersection(set(ma_proc_map))
 
     val_0 = [ma_map[h] for h in hm_comm]
     ground_truth = [ma_calc_map[h] for h in hm_comm]
@@ -69,18 +73,20 @@ if __name__=='__main__':
     unit_cell = (67.2, 59.8, 47.2, 90, 110.3, 90)
     space_group = 'C121'
 
-    # get environment variable WORK
+    # path to output of diffBragg stage 2
     input_path = os.environ["WORK"] + 'diffbragg_stage2/10217122'
-
+    # output of conventional merging mtz file
+    mtz_path = os.environ["SCRATCH"] + "/ferredoxin_sim/9521300/out/ly99sim_all.mtz" # output of conventional merging
     # Ground truth structure factors
     ma_calc = get_complex_fcalc_from_pdb(full_path("1m2a.pdb"),wavelength=1.3,dmin=1.9,dmax=1000).as_amplitude_array()
 
     # Miller index map
     f_asu_map=np.load(input_path + '/f_asu_map.npy',allow_pickle=True)[()]
 
-    mtz_path = os.environ["SCRATCH"] + "/ferredoxin_sim/9521300/out/ly99sim_all.mtz" # output of conventional merging
-    ma = iotbx.mtz.object(mtz_path).as_miller_arrays()[0]
-    pearson_coeff, val_0, ground_truth = evaluate_iter(ma, ma_calc)
+    # Output of conventional merging
+    ma_proc = iotbx.mtz.object(mtz_path).as_miller_arrays()[0]
+    ma_proc = ma_proc.as_amplitude_array()
+    pearson_coeff, val_0, ground_truth = evaluate_iter(ma_proc, ma_proc, ma_calc)
     
     all_iter_npz = len(glob.glob(input_path + '/_fcell_trial0_iter*.npz'))
 
@@ -94,10 +100,13 @@ if __name__=='__main__':
                         space_group,
                         save_mtz=True,
                         )
-        pearson_coeff, val_0, ground_truth = evaluate_iter(ma, ma_calc)
+        # import IPython
+        # IPython.embed()
+        pearson_coeff, val_0, ground_truth = evaluate_iter(ma, ma_proc, ma_calc)
         pearson_coeff_vec.append(pearson_coeff.statistic)
 
     # Plot pearson_coeff as a function of iteration
     plt.figure()
-    plt.plot(pearson_coeff_vec)
+    plt.plot(pearson_coeff_vec,".")
+    plt.savefig('pearson_coeff.png')
     plt.show()
