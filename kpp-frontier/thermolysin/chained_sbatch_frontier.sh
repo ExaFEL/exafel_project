ad061ba77be280e64f595400ac34d0620f0204f8
Iris Young <idyoung@lbl.gov>
2023-08-24 16:02:10 -0400

    Full-scale thermolysin test scripts on Frontier

--- kpp-frontier/thermolysin/chained_sbatch_frontier.sh
+++ kpp-frontier/thermolysin/chained_sbatch_frontier.sh
@@ -33,31 +33,31 @@ export SCRATCH=/lustre/orion/chm137/proj-shared/cctbx
 export SCRIPTS=$SCRATCH/thermolysin
 
 # Simulate 130k images. No dependencies.
-JOB_ID_SIM=$(chained_sbatch $SCRIPTS/thermolysin_test_sim.sh None)
+JOB_ID_SIM=$(chained_sbatch $SCRIPTS/thermolysin_500k_sim.sh None)
 
 # Index and integrate simulated images with dials.stills_process,
 # pending completion of image simulation. Note the first argument
 # is the dependency recognized by SLURM, and any remaining are
-# arguments passed to the thermolysin_test_*.sh script.
-JOB_ID_INDEX=$(chained_sbatch $SCRIPTS/thermolysin_test_index.sh $JOB_ID_SIM $JOB_ID_SIM)
+# arguments passed to the thermolysin_500k_*.sh script.
+JOB_ID_INDEX=$(chained_sbatch $SCRIPTS/thermolysin_500k_index.sh $JOB_ID_SIM $JOB_ID_SIM)
 
 # Merge stills processing results, pending completion of stills_process
-JOB_ID_MERGE=$(chained_sbatch $SCRIPTS/thermolysin_test_merge.sh $JOB_ID_INDEX $JOB_ID_INDEX)
+JOB_ID_MERGE=$(chained_sbatch $SCRIPTS/thermolysin_500k_merge.sh $JOB_ID_INDEX $JOB_ID_INDEX)
 
 # Split results of stills processing and prepare file list for diffBragg.
-JOB_ID_SPLIT=$(chained_sbatch $SCRIPTS/thermolysin_test_split.sh $JOB_ID_INDEX $JOB_ID_INDEX)
+JOB_ID_SPLIT=$(chained_sbatch $SCRIPTS/thermolysin_500k_split.sh $JOB_ID_INDEX $JOB_ID_INDEX)
 
 # Run diffBragg stage 1 ("hopper", local to each image).
 # Depends on stills process and merging results. Note
 # syntax bundling multiple jobids in the dependency argument.
-JOB_ID_HOPPER=$(chained_sbatch $SCRIPTS/thermolysin_test_stage1.sh "${JOB_ID_SPLIT}:${JOB_ID_MERGE}" $JOB_ID_SPLIT $JOB_ID_MERGE)
+JOB_ID_HOPPER=$(chained_sbatch $SCRIPTS/thermolysin_500k_stage1.sh "${JOB_ID_SPLIT}:${JOB_ID_MERGE}" $JOB_ID_SPLIT $JOB_ID_MERGE)
 
 # Complete spot prediction and interation of predictions with newly refined models.
-JOB_ID_PREDICT=$(chained_sbatch $SCRIPTS/thermolysin_test_predict.sh $JOB_ID_HOPPER $JOB_ID_HOPPER)
+JOB_ID_PREDICT=$(chained_sbatch $SCRIPTS/thermolysin_500k_predict.sh $JOB_ID_HOPPER $JOB_ID_HOPPER)
 
 # Run diffBragg stage 2 (global refinement of structure
 # factors against pixel data) on integration results.
-JOB_ID_STAGE2=$(chained_sbatch $SCRIPTS/thermolysin_test_stage2.sh $JOB_ID_PREDICT $JOB_ID_INDEX $JOB_ID_MERGE $JOB_ID_PREDICT)
+JOB_ID_STAGE2=$(chained_sbatch $SCRIPTS/thermolysin_500k_stage2.sh $JOB_ID_PREDICT $JOB_ID_INDEX $JOB_ID_MERGE $JOB_ID_PREDICT)
 
 echo "Job IDs to be executed sequentially:"
 echo "$JOB_ID_SIM image simulation"
---
+++ kpp-frontier/thermolysin/thermolysin_500k_split.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#SBATCH -N 1
+#SBATCH -J split
+#SBATCH -A CHM137
+#SBATCH -p batch
+#SBATCH -t 60
+#SBATCH -o %j.out
+#SBATCH -e %j.err
+export NTASKS=$((SLURM_JOB_NUM_NODES*28))
+export SRUN="srun -n $NTASKS --gpus-per-node=8 --cpus-per-gpu=7 --cpu-bind=cores"
+echo "splitting on $SLURM_JOB_NUM_NODES nodes with $SRUN"
+
+export JOB_ID_INDEX=$1
+
+export SCRATCH=/lustre/orion/chm137/proj-shared/cctbx
+export SCRATCH_FOLDER=$SCRATCH/thermolysin/$SLURM_JOB_ID
+
+echo "jobstart $(date)";pwd
+
+$SRUN diffBragg.make_input_file ${JOB_ID_INDEX} ${SLURM_JOB_ID}_integ_exp_ref.txt \
+    --splitDir ${SLURM_JOB_ID}/splits
+
+echo "jobend $(date)";pwd
