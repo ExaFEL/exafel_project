64e6a9ef0c29e6073e16e233f21f0f15d0116d68
Daniel Tchon <dantch@onet.eu>
2023-11-10 14:30:46 -0800

    Fixes to the heat plot tested on Perlmutter

--- post-validate/check_stage1/visualise_stage1_results.py
+++ post-validate/check_stage1/visualise_stage1_results.py
@@ -9,7 +9,7 @@ from itertools import islice
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from typing import Generator, List, Sequence, Tuple, TypeAlias, TypeVar
+from typing import Generator, List, Sequence, Tuple, TypeVar
 
 from exafel_project.kpp_eval.phil import parse_phil
 
@@ -33,10 +33,10 @@ y_key = ncells_dist
 """
 
 T = TypeVar('T')
-Stage1Results: TypeAlias = pd.DataFrame
+Stage1Results = pd.DataFrame
 
 
-def sliding_pairs(sequence: Sequence[T]) -> Generator[Tuple[T]]:
+def sliding_pairs(sequence: Sequence[T]) -> Generator[Tuple[T], None, None]:
     it = iter(sequence)
     window = deque(islice(it, 1), maxlen=2)
     for x in it:
@@ -57,33 +57,45 @@ def calculate_column_ranks(df: Stage1Results, key: str) -> Stage1Results:
 
 
 def plot_heatmap(df: Stage1Results, x_key: str, y_key: str) -> Stage1Results:
-    n_bins = np.log2(len(df))
+    n_bins = int(np.log2(len(df)))
     x_lims = (min(df[x_key]), max(df[x_key]) + 1e-9)
     y_lims = (min(df[y_key]), max(df[y_key]) + 1e-9)
     x_bins_lims = np.linspace(x_lims[0], x_lims[1], num=n_bins+1)
     y_bins_lims = np.linspace(y_lims[0], y_lims[1], num=n_bins+1)
     heat_data = np.zeros(shape=(n_bins, n_bins), dtype=int)
+    print(df[x_key].describe())
     df['__x_bin'] = df['__y_bin'] = 0
     for x_bins_max in x_bins_lims[1:]:
+        print(x_bins_max)
         df['__x_bin'] += df[x_key] > x_bins_max
     for y_bins_max in y_bins_lims[1:]:
         df['__y_bin'] += df[y_key] > y_bins_max
     for x_i, x_bin_lims in enumerate(sliding_pairs(x_bins_lims)):
-        for y_i, y_bins_lims in enumerate(sliding_pairs(y_bins_lims)):
-            heat_data[x_i, y_i] = sum((df[x_key] == x_i) & (df[y_key] == y_i))
-    x_bin_counts = heat_data.sum(axis=1)
-    y_bin_counts = heat_data.sum(axis=0)
-    fig, ((axx, axn), (axh, axy)) = plt.subplots(2, 2, sharex='True',
-        sharey=True, width_ratios=[2, 1], height_ratios=[1, 2])
-    axh.imshow(heat_data, cmap="Purples")
+        for y_i, y_bin_lims in enumerate(sliding_pairs(y_bins_lims)):
+            heat_data[x_i, y_i] = sum((df['__x_bin'] == x_i) & (df['__y_bin'] == y_i))
+    print(df['__x_bin'])
+    x_bin_counts = heat_data.sum(axis=0)
+    y_bin_counts = heat_data.sum(axis=1)
+    print('Head data x y')
+    print(heat_data.sum(axis=1))
+    print(heat_data.sum(axis=0))
+    fig, ((axx, axn), (axh, axy)) = plt.subplots(2, 2, sharex='col',
+        sharey='row', width_ratios=[2, 1], height_ratios=[1, 2])
+    plt.subplots_adjust(wspace=0, hspace=0)
+    print(heat_data)
+    axh.imshow(heat_data, cmap="Purples", origin='lower')
+    axh.set_aspect('auto')
     # axh.grid(which="minor", color="w", linestyle='-', linewidth=3)
+    print(x_bins_lims)
+    print(y_bins_lims)
     axh.set_xticks(np.arange(n_bins + 1) - 0.5, labels=x_bins_lims)
     axh.set_yticks(np.arange(n_bins + 1) - 0.5, labels=y_bins_lims)
     axh.set_xlabel(x_key)
     axh.set_ylabel(y_key)
     purple = plt.get_cmap('Purples')(1.0)
-    axx.bar(x=n_bins, height=x_bin_counts, width=1, color=purple)
-    axy.hbar(x=n_bins, height=y_bin_counts, width=1, color=purple)
+    axx.bar(x=range(n_bins), height=x_bin_counts, width=1, color=purple)
+    axy.barh(y=range(n_bins), width=y_bin_counts, height=1, color=purple)
+    axn.axis('off')
     plt.show()
 
 
