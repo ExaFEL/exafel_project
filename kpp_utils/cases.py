0c1e290e597abf4ad1b72275839c6cdc98d15fcb
Nicholas K Sauter <nksauter@lbl.gov>
2023-12-04 16:15:33 -0800

    Belongs with generalization

---
+++ kpp_utils/cases.py
@@ -0,0 +1,39 @@
+
+cases = dict (
+  cyto=dict(
+    pdb="exafel_project/kpp-frontier/cytochrome/5wp2.pdb",
+    off="30",
+    dmin="1.5",
+    spcgrp="P6522",
+    ucell="77.856,77.856,263.615,90,90,120",
+    cov="exafel_project/kpp-frontier/cytochrome/covariance_tdata_cells.pickle",
+    sigu="1 1",
+  ),
+  cry11ba=dict(
+    pdb="exafel_project/kpp-sim/cry11ba/7qyd.pdb",
+    off="160",
+    dmin="2.3",
+    spcgrp="P21212",
+    ucell="168.245,158.528,57.534,90,90,90",
+    cov="exafel_project/kpp-frontier/cry11ba/covariance_tdata_cells_20000.pickle",
+    sigu="1 1 1",
+  ),
+  thermo=dict(
+    pdb="exafel_project/kpp-sim/thermolysin/4tnl.pdb",
+    off="80",
+    dmin="1.7",
+    spcgrp="P6122",
+    ucell="93.0407,93.0407,130.41,90,90,120",
+    cov="exafel_project/kpp-sim/thermolysin/covariance_tdata_cells_cropped.pickle",
+    sigu="1 1",
+  ),
+  yb_lyso=dict(
+    pdb="exafel_project/kpp-frontier/yb_lyso/4bs7.pdb",
+    off="80",
+    dmin="1.7",
+    spcgrp="P43212",
+    ucell="79.1,79.1,38.4,90,90,90",
+    cov="exafel_project/kpp-sim/yb_lyso/covariance_tdata_cells.pickle",
+    sigu="1 1",
+  ),
+)
