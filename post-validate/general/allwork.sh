0c26d94147bc2a0afe06891f46cc0b6a83d0e7b3
Daniel Tchon <dantch@onet.eu>
2023-12-04 17:48:38 -0800

    Generalize `allwork.sh` and convert it to a batch script

---
+++ post-validate/general/allwork.sh
@@ -0,0 +1,22 @@
+#sample n_thousand length_microns
+BATCH_PATH=$MODULES/exafel_project/post-validate/general/batch.sh
+$BATCH_PATH cyto 65 40
+$BATCH_PATH cyto 65 25
+$BATCH_PATH cyto 65 10
+$BATCH_PATH cyto 65 5
+$BATCH_PATH cyto 65 2
+$BATCH_PATH cry11ba 65 16
+$BATCH_PATH cry11ba 65 8
+$BATCH_PATH cry11ba 65 4
+$BATCH_PATH cry11ba 65 2
+$BATCH_PATH cry11ba 65 0.5
+$BATCH_PATH thermo 65 20
+$BATCH_PATH thermo 65 10
+$BATCH_PATH thermo 65 5
+$BATCH_PATH thermo 65 2
+$BATCH_PATH thermo 65 1
+$BATCH_PATH yb_lyso 65 2
+$BATCH_PATH yb_lyso 65 1
+$BATCH_PATH yb_lyso 65 0.5
+$BATCH_PATH yb_lyso 65 0.25
+$BATCH_PATH yb_lyso 65 0.125
