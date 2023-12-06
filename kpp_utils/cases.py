70ecfa3f6ce9b1be31a728ee3f27903e9f01ee46
nksauter <nksauter@lbl.gov>
2023-12-05 16:07:44 -0800

    Update cases.py
    Bugfix, still need to verify in queue

--- kpp_utils/cases.py
+++ kpp_utils/cases.py
@@ -7,7 +7,7 @@ cases = dict (
     spcgrp="P6522",
     ucell="77.856,77.856,263.615,90,90,120",
     cov="exafel_project/kpp-frontier/cytochrome/covariance_tdata_cells.pickle",
-    sigu="1 1",
+    sigu="[1,1]",
   ),
   cry11ba=dict(
     pdb="exafel_project/kpp-sim/cry11ba/7qyd.pdb",
@@ -16,7 +16,7 @@ cases = dict (
     spcgrp="P21212",
     ucell="168.245,158.528,57.534,90,90,90",
     cov="exafel_project/kpp-frontier/cry11ba/covariance_tdata_cells_20000.pickle",
-    sigu="1 1 1",
+    sigu="[1,1,1]",
   ),
   thermo=dict(
     pdb="exafel_project/kpp-sim/thermolysin/4tnl.pdb",
@@ -25,7 +25,7 @@ cases = dict (
     spcgrp="P6122",
     ucell="93.0407,93.0407,130.41,90,90,120",
     cov="exafel_project/kpp-sim/thermolysin/covariance_tdata_cells_cropped.pickle",
-    sigu="1 1",
+    sigu="[1,1]",
   ),
   yb_lyso=dict(
     pdb="exafel_project/kpp-frontier/yb_lyso/4bs7.pdb",
@@ -34,6 +34,6 @@ cases = dict (
     spcgrp="P43212",
     ucell="79.1,79.1,38.4,90,90,90",
     cov="exafel_project/kpp-sim/yb_lyso/covariance_tdata_cells.pickle",
-    sigu="1 1",
+    sigu="[1,1]",
   ),
 )
