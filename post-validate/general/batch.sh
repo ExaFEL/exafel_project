f75182f1f6c595e0b1a9eeffb000c2abfab3e2e9
Nicholas K Sauter <nksauter@lbl.gov>
2023-12-06 16:10:17 -0800

    Backport runtime estimates from PR#30

--- post-validate/general/batch.sh
+++ post-validate/general/batch.sh
@@ -37,12 +37,12 @@ echo "w/ unrestrained: "$n_unrestrain
 
 mkdir -p ${odir}
 # times calibrated for 1 node per 1024 shots
-sbatch -J$job $GPU -t 50 -o${odir}/sim.out -e${odir}/sim.err ${script_dir}/general_sim.sh $length $nshot $detdist $pdb $odir
-sbatch -J$job $CPU -t 60 -o${odir}/idx.out -e${odir}/idx.err ${script_dir}/general_index.sh $dmin $spcgrp $ucell $odir
-sbatch -J$job $CPU -t 20 -o${odir}/mrg.out -e${odir}/mrg.err ${script_dir}/general_merge.sh $cov $pdb $dmin $odir
-sbatch -J$job $CPU -t 20 -o${odir}/spl.out -e${odir}/spl.err ${script_dir}/general_split.sh $odir
-sbatch -J$job $GPU -t 60 -o${odir}/st1.out -e${odir}/st1.err ${script_dir}/general_stage1_restraints_3fold.sh $sigu $spcgrp $odir ${n_unrestrain}
-sbatch -J$job $GPU -t 60 -o${odir}/prd.out -e${odir}/prd.err ${script_dir}/general_predict.sh $dmin $spcgrp $ucell $odir
+sbatch -J$job $GPU -t 40  -o${odir}/sim.out -e${odir}/sim.err ${script_dir}/general_sim.sh $length $nshot $detdist $pdb $odir
+sbatch -J$job $CPU -t 15  -o${odir}/idx.out -e${odir}/idx.err ${script_dir}/general_index.sh $dmin $spcgrp $ucell $odir
+sbatch -J$job $CPU -t 10  -o${odir}/mrg.out -e${odir}/mrg.err ${script_dir}/general_merge.sh $cov $pdb $dmin $odir
+sbatch -J$job $CPU -t 5   -o${odir}/spl.out -e${odir}/spl.err ${script_dir}/general_split.sh $odir
+sbatch -J$job $GPU -t 60  -o${odir}/st1.out -e${odir}/st1.err ${script_dir}/general_stage1_restraints_3fold.sh $sigu $spcgrp $odir ${n_unrestrain}
+sbatch -J$job $GPU -t 60  -o${odir}/prd.out -e${odir}/prd.err ${script_dir}/general_predict.sh $dmin $spcgrp $ucell $odir
 sbatch -J$job $GPU -t 120 -o${odir}/st2.out -e${odir}/st2.err ${script_dir}/general_stage2.sh $dmin $odir
 # optional ens.hopper:
 sbatch -J$job $GPU -t 120 -o${odir}/ens.out -e${odir}/ens.err ${script_dir}/general_ensHopper.sh $dmin $odir
