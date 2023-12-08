9581729076a822c0c45e10517562faf8469ccfe9
Daniel Tchon <dantch@onet.eu>
2023-12-08 14:02:32 -0800

    Sbatch selected scripts with optional $4 instead of manually commenting

--- post-validate/general/batch.sh
+++ post-validate/general/batch.sh
@@ -11,9 +11,12 @@ spcgrp=`libtbx.python -c "from exafel_project.kpp_utils.cases import cases; prin
 ucell=`libtbx.python -c "from exafel_project.kpp_utils.cases import cases; print(cases['$sample']['ucell'])"`
 cov=`libtbx.python -c "from exafel_project.kpp_utils.cases import cases; print(cases['$sample']['cov'])"`
 sigu=`libtbx.python -c "from exafel_project.kpp_utils.cases import cases; print(cases['$sample']['sigu'])"`
-
-
 echo "Initializing diffBragg analysis of $n_thousand thousands of $sample crystals, $length um in length,"
+
+# If $4 is given, it must be a string of digits including steps numbers to run:
+# "1" for sim, ..., "7" for stage2, "8" for ens_hopper. By default 1-7 are run.
+requested_step=${4:-1234567}
+requested() { [[ $requested_step =~ $1 ]]; }
 
 # SLURM settings
 Q=regular  # NERSC queue
@@ -36,12 +39,12 @@ echo "Number of shots: "$nshot
 echo "w/ unrestrained: "$n_unrestrain
 
 # times calibrated for 1 node per 1024 shots
-sbatch -J$job $GPU -t 40  -o${odir}/sim.out -e${odir}/sim.err ${script_dir}/general_sim.sh $length $nshot $detdist $pdb $odir
-sbatch -J$job $CPU -t 15  -o${odir}/idx.out -e${odir}/idx.err ${script_dir}/general_index.sh $dmin $spcgrp $ucell $odir
-sbatch -J$job $CPU -t 10  -o${odir}/mrg.out -e${odir}/mrg.err ${script_dir}/general_merge.sh $cov $pdb $dmin $odir
-sbatch -J$job $CPU -t 5   -o${odir}/spl.out -e${odir}/spl.err ${script_dir}/general_split.sh $odir
-sbatch -J$job $GPU -t 60  -o${odir}/st1.out -e${odir}/st1.err ${script_dir}/general_stage1_restraints_3fold.sh $sigu $spcgrp $odir ${n_unrestrain}
-sbatch -J$job $GPU -t 60  -o${odir}/prd.out -e${odir}/prd.err ${script_dir}/general_predict.sh $dmin $spcgrp $ucell $odir
-sbatch -J$job $GPU -t 120 -o${odir}/st2.out -e${odir}/st2.err ${script_dir}/general_stage2.sh $dmin $odir
+if requested 1; then sbatch -J$job $GPU -t 40  -o${odir}/sim.out -e${odir}/sim.err ${script_dir}/general_sim.sh $length $nshot $detdist $pdb $odir ; fi
+if requested 2; then sbatch -J$job $CPU -t 15  -o${odir}/idx.out -e${odir}/idx.err ${script_dir}/general_index.sh $dmin $spcgrp $ucell $odir ; fi
+if requested 3; then sbatch -J$job $CPU -t 10  -o${odir}/mrg.out -e${odir}/mrg.err ${script_dir}/general_merge.sh $cov $pdb $dmin $odir ; fi
+if requested 4; then sbatch -J$job $CPU -t 5   -o${odir}/spl.out -e${odir}/spl.err ${script_dir}/general_split.sh $odir ; fi
+if requested 5; then sbatch -J$job $GPU -t 60  -o${odir}/st1.out -e${odir}/st1.err ${script_dir}/general_stage1_restraints_3fold.sh $sigu $spcgrp $odir ${n_unrestrain} ; fi
+if requested 6; then sbatch -J$job $GPU -t 60  -o${odir}/prd.out -e${odir}/prd.err ${script_dir}/general_predict.sh $dmin $spcgrp $ucell $odir ; fi
+if requested 7; then sbatch -J$job $GPU -t 120 -o${odir}/st2.out -e${odir}/st2.err ${script_dir}/general_stage2.sh $dmin $odir ; fi
 # optional ens.hopper:
-sbatch -J$job $GPU -t 120 -o${odir}/ens.out -e${odir}/ens.err ${script_dir}/general_ensHopper.sh $dmin $odir
+if requested 8; then sbatch -J$job $GPU -t 120 -o${odir}/ens.out -e${odir}/ens.err ${script_dir}/general_ensHopper.sh $dmin $odir ; fi
